3c03e79e51485feae0fad964744ff8d21f66cf93
jhigs <flores.gideon1@gmail.com>
2020-11-15 22:23:28 +0800

    Add comment

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-Mixed Message
+# Mixed Message - Random message 
+
+> The main objective of project is to create a random message every time the user execute program.
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Features](#features)
+* [Status](#status)
+* [Inspiration](#inspiration)
+
+## General info
+Mixed Message is a console app that prints a randomly generated message. The message that it generates is came from the collection of statements stored in the program. 
+
+## Technologies
+* Javascript 
+* Node js - version 14.15.0
+* git - version 2.29.2.windows.2
+
+## Features
+List of features ready and TODOs for future development
+* Prints a random statement.
+
+## Status
+Ongoing project.
+
+## Inspiration
+This is a portfolio project in Codecademy.
+
--- messageGenerator.js
+++ messageGenerator.js
@@ -1,7 +1,14 @@
-const messages = ["Today you will be blessed!","You will meet some one that will make change in your way of life.","A streesed-free day for you! ","Wishing you a safe and healthy December.","Your past does not define your future.","It's time to grind and hone your skills","Life is full of surprises, open yours!."];
-const randomNumbers = Math.floor(Math.random()*7);
+// Array message that store a number of statement.
+const messages = ["Today you will be blessed!",
+        "You will meet some one that will make change in your way of life.",
+        "A streesed-free day for you! ","Wishing you a safe and healthy December.",
+        "Your past does not define your future.","It's time to grind and hone your skills",
+        "Life is full of surprises, open yours!."]
 
-
-console.log(messages[randomNumbers]);
-
-
+// function that pull an element in messages array randomly
+const giveMessage = () =>{
+    let newMessage = messages[Math.floor(Math.random()*messages.length)];
+    return newMessage;
+}
+// prints the returned string by the giveMessage()
+console.log(giveMessage());
