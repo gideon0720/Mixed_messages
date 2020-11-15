089844ff2658cba74ed8a15f6c0f32de12579371
jhigs <flores.gideon1@gmail.com>
2020-11-15 17:10:40 +0800

    Add README.md & messageGenerator.js

---
+++ messageGenerator.js
@@ -0,0 +1,7 @@
+const messages = ["Today you will be blessed!","You will meet some one that will make change in your way of life.","A streesed-free day for you! ","Wishing you a safe and healthy December.","Your past does not define your future.","It's time to grind and hone your skills","Life is full of surprises, open yours!."];
+const randomNumbers = Math.floor(Math.random()*7);
+
+
+console.log(messages[randomNumbers]);
+
+
